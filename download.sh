76b1f1e0c4cf786e097310ecf26b4fc2b82e69a7
Kerk en IT <info@kerkenit.nl>
2022-06-16 17:12:52 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 account=`cat /boot/kerktv/account`
 church=`cat /boot/kerktv/church`
-wget -O /home/marcovantklooster/loader.png "https://api.promissa.nl/vid/$account/$church/loader.png"
-wget -O "/home/marcovantklooster/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
+FLAG="/var/log/firstboot.log"
+if [[ ! -f $FLAG ]]; then
+	wget -O "/home/$USER/loader.png" "https://api.promissa.nl/vid/$account/$church/loader.png" && touch "$FLAG"
+fi
+
+wget -O "/var/log/kerktv/$account.md5" "https://api.promissa.nl/vid/$account/$church/$account.md5"
+sed -i "s/$church.mp4/\/home\/$USER\/video\/$church.mp4/" "/var/log/kerktv/$account.md5"
+md5sum -c "/var/log/kerktv/$account.md5" || wget -O "/home/$USER/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
