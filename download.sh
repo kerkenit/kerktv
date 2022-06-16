a33b7ea26973d0b023efa4dd1323947ca095256a
Kerk en IT <info@kerkenit.nl>
2022-06-16 17:21:51 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 account=`cat /boot/kerktv/account`
 church=`cat /boot/kerktv/church`
-FLAG="/var/log/firstboot.log"
-if [[ ! -f $FLAG ]]; then
-	wget -O "/home/$USER/loader.png" "https://api.promissa.nl/vid/$account/$church/loader.png" && touch "$FLAG"
+MD5_PATH="/var/log/kerktv/$account.md5"
+if [[ ! -f $MD5_PATH ]]; then
+	cd "/home/$USER/kerktv/" && git pull
+	wget -O "/home/$USER/loader.png" "https://api.promissa.nl/vid/$account/$church/loader.png"
 fi
-
-wget -O "/var/log/kerktv/$account.md5" "https://api.promissa.nl/vid/$account/$church/$account.md5"
-sed -i "s/$church.mp4/\/home\/$USER\/video\/$church.mp4/" "/var/log/kerktv/$account.md5"
-md5sum -c "/var/log/kerktv/$account.md5" || wget -O "/home/$USER/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
+wget -O "$MD5_PATH" "https://api.promissa.nl/vid/$account/$church/$account.md5"
+sed -i "s/$church.mp4/\/home\/$USER\/video\/$church.mp4/" "$MD5_PATH"
+md5sum -c "$MD5_PATH" || wget -O "/home/$USER/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
