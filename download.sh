34666e3d18735f00cf61a4b0bb8ebfc369d0c3ff
Kerk en IT <info@kerkenit.nl>
2022-06-17 20:31:12 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,11 +5,10 @@ MD5_PATH="/var/log/kerktv/$account.md5"
 if [[ ! -f $MD5_PATH ]]; then
 	wget -O "/home/$USER/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
 	wget -O "$MD5_PATH" "https://api.promissa.nl/vid/$account/$church/$account.md5" && sed -i "s/$church.mp4/\/home\/$USER\/video\/$church.mp4/" "$MD5_PATH"
-	sudo "/home/$USER/pi_video_looper/reload.sh"
+	sudo bash "/home/$USER/pi_video_looper/reload.sh"
 	cd "/home/$USER/kerktv/" && git reset --hard
 	cd "/home/$USER/kerktv/" && git pull
 	sudo chmod 755 "/home/$USER/kerktv/*.sh"
-#	sudo bash "/home/$USER/pi_video_looper/reload.sh"
 else
 	wget -O "$MD5_PATH" "https://api.promissa.nl/vid/$account/$church/$account.md5" &&sed -i "s/$church.mp4/\/home\/$USER\/video\/$church.mp4/" "$MD5_PATH"
 	md5sum -c "$MD5_PATH" || wget -O "/home/$USER/video/$church.mp4" "https://api.promissa.nl/vid/$account/$church/$church.mp4"
